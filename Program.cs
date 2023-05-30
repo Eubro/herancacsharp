4b8c4fb51497e467ca7dd0e63caaf70a6191ca0c
Elias Junior <eliasjunior1405@gmail.com>
2023-05-30 08:59:25 -0300

    criadas classes para desenvolvimento de herança

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account= new BusinessAccount(8010,"Bob", 100, 500);
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
+
+
